601edb315d7588ef30b9757ee782fefccc707582
sepehr kianian <sepehr kianian>
2023-06-06 16:28:41 +0330

    feat(core): init

---
+++ store/index.js
@@ -0,0 +1,7 @@
+import { vuexCookiePersistence } from '~/utils/vuexCookiePersistence'
+
+export const plugins = [vuexCookiePersistence.plugin]
+
+export const state = () => ({})
+
+export const mutations = {}
