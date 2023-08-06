5eb7e5e661d3858fab0e40d25d5b0a3dd2e3432e
Mohammad Abolnejadian <mohammad.abolnejadian@gmail.com>
2023-08-06 03:53:46 +0330

    welcome page done.

---
+++ pages/index/welcome.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="welcome-page">
+        <center>
+            <div class="header">
+                به شبکه اجتماعی
+                <img src="@/static/images/logo-withtypography.svg" alt="Ghasedak Logo" class="logo-withtypography">
+                خوش آمدید
+            </div>
+        </center>
+        <div class="top-illustration">
+            <img src="@/static/images/top-welcome-illustration.svg" alt="Two women working with their phone">
+        </div>
+        <div class="intro-boxes">
+            <div class="top-intro">
+                <div class="top-intro-box intro-box"><img src="@/static/images/intro-top.svg"
+                        alt="Share with people in channels" class="top-intro-icon">
+                    محتوای خود را در کانال‌های خود به اشتراک بگذارید
+                </div>
+            </div>
+            <div class="middle-intro">
+                <div class="middle-intro-box intro-box">
+                    <img src="@/static/images/cash.svg" alt="Monetize your channel" class="middle-intro-icon">
+                    با ایجاد کانال، کسب درآمد کنید
+                </div>
+            </div>
+            <div class="bottom-intro">
+                <div class="bottom-intro-box intro-box">
+                    <img src="@/static/images/join.svg" alt="Find your favorite channels" class="bottom-intro-icon">
+                    کانال‌های مورد علاقه خود را دنبال کنید
+                </div>
+            </div>
+        </div>
+        <center>
+            <div class="actions">
+                <div class="signup-button">
+                    <img src="@/static/images/chevron-left.svg" alt="Back" class="signup-button-icon">
+                    <p class="signup-button-text">شروع کنید</p>
+                </div>
+                <div class="signin-section">
+                    <a href="#" class="signin-button">وارد شوید</a>
+                    <p class="signin-section-text">اکانت دارید؟</p>
+                </div>
+            </div>
+        </center>
+        <div class="bottom-illustration">
+            <img src="@/static/images/bottom-welcome-illustration.svg" alt="Woman lying on phone">
+        </div>
+    </div>
+</template>
+  
+<script lang="ts">
+</script>
+  
+<style>
+@import "~/stylesheets/welcome.css";
+</style>
+  
