7a7bb6d4359b8fc3272215993a488ccb8f37e490
Sepehr Kianian <sepehrkianian09@yahoo.com>
2023-08-19 22:28:33 +0330

    feat(channel): init channel-role-component

--- components/ChannelRoleComponent.vue
+++ components/ChannelRoleComponent.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="role row">
-    <img src="@/static/images/dollar-coin.svg" class="image-sized--1">
-    <h5>{{ 'عضو ویژه' }}</h5>
+  <div v-if="shouldShow" class="role row">
+    <img :src="getImageSrc(roleConfig.icon)" class="image-sized--1">
+    <h5>{{ roleConfig.title }}</h5>
   </div>
 </template>
 
@@ -11,9 +11,32 @@ import Vue from 'vue'
 import { Prop } from 'vue-property-decorator'
 import { ChannelRole } from '~/models'
 
+interface RoleConfig {
+  icon: string;
+  title: string;
+}
+
+const RoleConfigs: Partial<{[key in ChannelRole]: RoleConfig}> = {
+  [ChannelRole.OWNER]: { icon: 'owner', title: 'صاحب' },
+  [ChannelRole.ADMIN]: { icon: 'admin', title: 'مدیر' },
+  [ChannelRole.SPECIAL_MEMBER]: { icon: 'special-member', title: 'عضو ویژه' }
+}
+
 @Component
 export default class ChannelRoleComponent extends Vue {
     @Prop() readonly role: ChannelRole
+
+    get roleConfig (): RoleConfig {
+      return RoleConfigs[this.role]
+    }
+
+    get shouldShow (): boolean {
+      return this.role !== ChannelRole.MEMBER
+    }
+
+    getImageSrc (roleIcon: string) {
+      return require(`@/static/images/channel-roles/${roleIcon}.svg`)
+    }
 }
 </script>
 
