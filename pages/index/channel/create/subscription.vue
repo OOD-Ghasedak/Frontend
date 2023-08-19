ea37a1b90b6e9cbd6f54f7bb0132e8eb4aa11dd0
Sepehr Kianian <sepehrkianian09@yahoo.com>
2023-08-19 21:51:47 +0330

    feat(channel): init define-subscription page

---
+++ pages/index/channel/create/subscription.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="create-channel-define-subscription-page" />
+</template>
+
+<script lang="ts">
+import Component from 'vue-class-component'
+import RootComponent from '~/utils/rootComponent'
+
+@Component
+export default class CreateChannelDefineSubscriptionPage extends RootComponent {
+}
+</script>
+<router>
+{name: "CreateChannelDefineSubscriptionPage"}
+</router>
+
+<style>
+</style>
