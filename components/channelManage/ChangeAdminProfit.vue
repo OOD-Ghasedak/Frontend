5a94b5c03df8b22815611aa01664a62ac609da2d
Sepehr Kianian <sepehrkianian09@yahoo.com>
2023-08-21 22:07:42 +0330

    refactor(modals)

---
+++ components/channelManage/ChangeAdminProfit.vue
@@ -0,0 +1,51 @@
+<template>
+  <b-modal centered visible size="lg" class="buy-sub">
+    <template #default>
+      <div class="modal-main promote-to-admin-main">
+        <div class="content row">
+          <img src="@/static/images/ghased.svg" class="image-sized-1">
+          <div class="names">
+            <h5>hi</h5>
+            <h6>@hello</h6>
+          </div>
+        </div>
+
+        <div class="profit-part">
+          <h4 class="profit-text">
+            عواید
+          </h4>
+          <input type="number" name="profit" placeholder="درصد عواید" class="profit-input">
+          <p>درصد</p>
+        </div>
+      </div>
+    </template>
+    <template #modal-footer>
+      <div class="ok-button">
+        <button class="primary-button">
+          <h5>{{ 'تغییر درصد' }}</h5>
+          <img src="@/static/images/exchange.svg" class="image-sized--2">
+        </button>
+      </div>
+    </template>
+    <template #modal-header-close>
+      <div class="close-button">
+        <h5 class="close-text">
+          انصراف
+        </h5>
+        <img src="@/static/images/close.svg" alt="" class="close-icon">
+      </div>
+    </template>
+  </b-modal>
+</template>
+
+<script lang="ts">
+import Component from 'vue-class-component'
+import Vue from 'vue'
+
+  @Component
+export default class ChangeAdminProfit extends Vue {
+}
+</script>
+
+  <style>
+  </style>
